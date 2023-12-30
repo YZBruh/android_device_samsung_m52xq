b2a0b7ca8b3d5bb71639c1ac12be3f4d541d6b7d
YZBruh <yagizzengin73@gmail.com>
2023-12-30 22:45:02 +0300

    Adapt ArrowOS

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_m52xq.mk
+    $(LOCAL_DIR)/arrow_m52xq.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_m52xq-userdebug \
-    lineage_m52xq-user \
-    lineage_m52xq-eng
+    arrow_m52xq-userdebug \
+    arrow_m52xq-user \
+    arrow_m52xq-eng
